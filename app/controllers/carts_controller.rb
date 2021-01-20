02e45ecda487ace4c22a31e6c17ae504d660f8cc
Tomohiro <tmitani@heroku.com>
2021-01-20 12:16:03 +0900

    minor fix for security

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,6 @@
 class CartsController < ApplicationController
+  before_action :set_products_order, only: [:update, :delete]
+
   def new
     if current_user.orders.where(state: 'cart').empty?
       @cart_order = Order.create!(state: 'cart', user: current_user)
@@ -8,21 +10,26 @@ class CartsController < ApplicationController
   end
 
   def update
-    @new_products_order = ProductsOrder.find(params['products_order'].to_i)
-    if params['sign'] == 'plus'
-      @new_products_order.quantity += 1
-    elsif params['sign'] == 'minus' && @new_products_order.quantity > 1
-      @new_products_order.quantity -= 1
+    if params['sign'].to_i == 1
+      @target_products_order.quantity += 1
+    elsif params['sign'].to_i == -1 && @target_products_order.quantity > 1
+      @target_products_order.quantity -= 1
     end
-    @new_products_order&.save
+    @target_products_order&.save
     render 'carts/update_product'
   end
 
   def delete
-    @deleting_products_order_id = ProductsOrder.find(params['products_order'].to_i).id
-    ProductsOrder.find(params['products_order'].to_i).delete
+    @target_products_order.delete
     @only_product = true
     @only_product = false if current_user.orders.where(state: 'cart').first.products_orders.exists?
     render 'carts/delete_product'
+  end
+
+  private
+
+  def set_products_order
+    @target_products_order = current_user.orders.where(state: 'cart').first
+                                         .products_orders.find(params['products_order'].to_i)
   end
 end
