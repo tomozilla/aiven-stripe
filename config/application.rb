7ffa1a8323a6cf331f2d025b4af1b198811f55f3
Tomohiro Mitani <tmitani@stripe.com>
2021-10-14 17:13:56 +0900

    update minor details

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module MiniProject
       generate.assets false
       generate.helper false
       generate.test_framework :test_unit, fixture: false
+      config.web_console.whiny_requests = false
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
